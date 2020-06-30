a2da0ac09d22363938df9fb72c167d5f67edf353
Noah Litwiller <litwillernoah@gmail.com>
2020-06-30 15:43:40 -0500

    Recreated app with scss, generated components

--- noahlitwiller-github-io/src/app/app.module.ts
+++ noahlitwiller-github-io/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
+import { HomeComponent } from './modules/home/home.component';
+import { ProjectsComponent } from './modules/projects/projects.component';
+import { ResumeComponent } from './modules/resume/resume.component';
+import { ContactComponent } from './modules/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ResumeComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
