5eaa6651728c0b6ade362787356f5c01d0f23a96
Noah Litwiller <litwillernoah@gmail.com>
2021-05-26 13:50:34 -0500

    initial commit

--- noahlitwiller-github-io/src/app/app.module.ts
+++ noahlitwiller-github-io/src/app/app.module.ts
@@ -1,28 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { ProjectsComponent } from './modules/projects/projects.component';
+import { ContactComponent } from './modules/contact/contact.component';
+import { ResumeComponent } from './modules/resume/resume.component';
 import { HeaderComponent } from './templates/header/header.component';
 import { FooterComponent } from './templates/footer/footer.component';
 import { HomeComponent } from './modules/home/home.component';
-import { ProjectsComponent } from './modules/projects/projects.component';
-import { ResumeComponent } from './modules/resume/resume.component';
-import { ContactComponent } from './modules/contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProjectsComponent,
+    ContactComponent,
+    ResumeComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
-    ProjectsComponent,
-    ResumeComponent,
-    ContactComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- noahlitwiller-github-io/src/app/templates/header/header.component.ts
+++ noahlitwiller-github-io/src/app/templates/header/header.component.ts
@@ -1,4 +1,6 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  curr_route = "/";
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private router: Router) { 
+    router.events.subscribe( (event) => ( event instanceof NavigationEnd ) && this.updateCurrRoute() )
   }
+
+  updateCurrRoute = () => {
+    this.curr_route = this.router.url;
+  };
+
+  isCurrRoute(route: String){
+    return route == this.curr_route;
+  }
+  
+
+  ngOnInit(): void {  }
 
 }
